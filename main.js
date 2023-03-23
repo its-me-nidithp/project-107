17811a4f47e338a87c5bdc0b4989168f73147508
its-me-nidithp <96280982+its-me-nidithp@users.noreply.github.com>
2023-03-23 16:27:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function start(){
+
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/6N-FySGWP/model.json", modelReady);
+    
+    }
+    
+    function modelReady(){
+    
+        classifier.classify(gotResults);
+    
+    }
+    
